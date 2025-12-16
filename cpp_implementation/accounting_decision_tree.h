d16cacc30b5cb4862da0d4b636193c362f76d6a6
Piyush Choudhari <choudhari.piyush@gmail.com>
2025-12-16 07:17:52 +0000

    update preprocessing directive

--- cpp_implementation/accounting_decision_tree.h
+++ cpp_implementation/accounting_decision_tree.h
@@ -1,5 +1,4 @@
-#ifndef DECISION_TREE_H
-#define DECISION_TREE_H
+#pragma once
 
 #include <iostream>
 #include <memory>
@@ -45,10 +44,10 @@ class OutcomeNode : public Node {
 private:
     Result value_;
     Action action_;
-    
+
 public:
     OutcomeNode(Result value, Action action = nullptr);
-    
+
     Result evaluate(const Context& context) const override;
     std::string getType() const override;
     std::string toJson(int indent = 0) const override;
@@ -60,17 +59,17 @@ private:
     Condition condition_;
     NodePtr trueNode_;
     NodePtr falseNode_;
-    
+
 public:
-    DecisionNode(const std::string& name, 
+    DecisionNode(const std::string& name,
                  Condition condition,
                  NodePtr trueNode = nullptr,
                  NodePtr falseNode = nullptr);
-    
+
     Result evaluate(const Context& context) const override;
     std::string getType() const override;
     std::string toJson(int indent = 0) const override;
-    
+
     void setTrueNode(NodePtr node);
     void setFalseNode(NodePtr node);
 };
@@ -80,13 +79,13 @@ private:
     std::string name_;
     std::vector<std::pair<Condition, NodePtr>> branches_;
     NodePtr defaultNode_;
-    
+
 public:
     explicit MultiBranchNode(const std::string& name);
-    
+
     MultiBranchNode& addBranch(Condition condition, NodePtr node);
     MultiBranchNode& setDefault(NodePtr node);
-    
+
     Result evaluate(const Context& context) const override;
     std::string getType() const override;
     std::string toJson(int indent = 0) const override;
@@ -96,10 +95,10 @@ class DecisionTreeEngine {
 private:
     NodePtr root_;
     mutable std::vector<std::string> trace_;
-    
+
 public:
     explicit DecisionTreeEngine(NodePtr root);
-    
+
     Result evaluate(const Context& context, bool enableTrace = false);
     const std::vector<std::string>& getTrace() const;
     void printTree() const;
@@ -109,5 +108,3 @@ std::string resultToString(const Result& result);
 
 void loanApprovalExample();
 void riskAssessmentExample();
-
-#endif
---
+++ run.sh
@@ -0,0 +1,1 @@
+g++ -o accounting_decision_tree cpp_implementation/accounting_decision_tree.cpp cpp_implementation/main.cpp
